ee310ff95547c298dcf0a68c24d4f3dd67c67d25
Jessica Berry <jberry@pop-os.localdomain>
2024-02-27 12:28:35 -0700

    a git commit without all the big uneccessary files

--- vbudsunix/exportpath.sh
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=/usr/local/MATLAB/R2011a/bin/glnxa64/
--- vbudsunix/get_case.m
+++
@@ -1,6 +0,0 @@
-function case_data = get_case(load_case);
-
-file_name=horzcat('save',num2str(load_case),'.mat');
-a=open(file_name);
-case_data=a.data;
-
